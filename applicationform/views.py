b435b41c933ee58f55831b4c1eae7882aeb48bce
Romain LEFRANC <rlefranc@lefresnoy.net>
2016-10-21 17:45:57 +0200

    send an email in local temp

--- applicationform/views.py
+++ applicationform/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+# -*- encoding: utf-8 -*-
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.http import HttpResponse
 from forms import PostForm
+from django.core.mail import send_mail, BadHeaderError
 
 
 def index(request):
@@ -10,8 +12,37 @@ def index(request):
 
 
 def form_new(request):
-    form = PostForm()
+
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+
+            subject = "Inscription " # form.cleaned_data['subject']
+            from_email = form.cleaned_data['email']
+
+            message = ""
+
+            for field in form:
+                message += '<p><strong>'+str(field.label)+'</strong>:'+str(field.value())+'</p>'
+
+            
+            try:
+                send_mail(subject, message, from_email, ['rlefranc@lefresnoy.net'])
+
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+
+            return HttpResponse("OK")
+
+
+
+
+            #return redirect('applicationform.views.post_send_detail', pk=post.pk)
+    else:
+        form = PostForm()
+
     return render(request, 'form/form_new.html', {'form': form})
+
 
 
 def form_list(request):
--- kartin/settings.py
+++ kartin/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = '/tmp/app-messages' # change this to a proper location
