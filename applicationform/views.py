95917ebea713da86f2e8a024bcbf7f029d4dfb6d
Romain LEFRANC <rlefranc@lefresnoy.net>
2016-10-21 16:50:13 +0200

    start basics templates

--- applicationform/views.py
+++ applicationform/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    #return render(request, 'applicationform/templates/base.html', {})
+     html = "<html><body>It is now </body></html>"
+     return HttpResponse(html)
+
+
+def form_new(request):
+    form = PostForm()
+    return render(request, 'form/form_new.html', {'form': form})
--- kartin/urls.py
+++ kartin/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^application/', include('applicationform.urls')),
+    url(r'^$', 'applicationform.views.index', name='index'),
 ]
